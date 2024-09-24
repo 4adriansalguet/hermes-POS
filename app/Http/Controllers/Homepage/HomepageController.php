e6b8567edc507d9a2f21fa314b42a98f9f6830b3
CreativeOutline <team@creativeoutline.co.uk>
2024-09-24 14:57:42 +0800

    Initial commit

---
+++ app/Http/Controllers/Homepage/HomepageController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Homepage;
+
+use App\Http\Controllers\Controller;
+use App\Mail\AppointmentEmail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class HomepageController extends Controller
+{
+    public function index()
+    {
+        return view('homepage.homepage');
+    }
+    public function appointmentMiking()
+    {
+        return view('homepage.makeappointment');
+    }
+    public function createAppointmentSchedule(Request $request)
+    {
+        $request->validate([
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => 'required',
+            'phone' => 'required',
+            'address' => 'required',
+            'issue' => 'required',
+            'problem' => 'required',
+            'device_type' => 'required',
+            'device_model' => 'required',
+            'devicePhoto' => 'required|Image|mimes:jpg,png'
+        ]);
+
+        if($request->price == 'Price will be discuss by <a href='){
+            $price = 'Will be discuss.';
+        }else{
+            $price = $request->price;
+        }
+
+        $data = [
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'price' => $price,
+            'issue' => $request->issue,
+            'problem' => $request->problem,
+            'device_type' => $request->device_type,
+            'device_model' => $request->device_model,
+            'devicePhoto' => $request->devicePhoto
+        ];
+
+        Mail::to('o.lermovenz@gmail.com')->send(new AppointmentEmail($data));
+        return redirect(route('thankyou'));
+    }
+
+    public function Thankyou()
+    {
+        return view('homepage.thankyou');
+    }
+}
